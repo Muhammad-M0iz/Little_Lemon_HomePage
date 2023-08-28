4ee731012919d1923cdd03b0c8d99acb37eb18a8
moiz <muhammadmoiz300@gmail.com>
2023-08-28 12:27:11 +0500

    Little Lemon Home page v3 , added TopAppbar along with nav drawer Fix access to the main screen added lazy Column Updated TopAppBar Logo with no bg Next update Contents 1) New Order Screen 2) Navigation 3) Login Screen

--- app/src/main/java/com/example/homescreen/TopAppBar.kt
+++ app/src/main/java/com/example/homescreen/TopAppBar.kt
@@ -1,9 +1,14 @@
 package com.example.homescreen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Face
@@ -24,8 +29,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 
 
@@ -60,9 +69,22 @@ fun TopNavigation() {
         }) {
         Scaffold(
             topBar = {
+
+
                 TopAppBar(title = {
-                    Text(text = "Little Lemon")
-                }, navigationIcon = {
+                    Box(modifier = Modifier.fillMaxWidth()){
+                        Image(painterResource(id = R.drawable.logo),"menu",
+                        modifier= Modifier
+                            .size(150.dp)
+                            .align(Center)
+                            .padding(end = 40.dp)
+                        )
+
+
+                    }
+                },
+
+                    navigationIcon = {
                     IconButton(onClick = {
                         scope.launch {
                             drawerState.open()
@@ -74,6 +96,7 @@ fun TopNavigation() {
                         )
                     }
                 }
+
 
                 )
             }
@@ -87,6 +110,7 @@ fun TopNavigation() {
 
     }
 }
+
 
 
 
