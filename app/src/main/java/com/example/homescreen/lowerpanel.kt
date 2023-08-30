46da1c265aadbee2d390a1626e611a43a7c4ae67
moiz <muhammadmoiz300@gmail.com>
2023-08-30 22:46:50 +0500

    Little Lemon Home page v6, Improved ui

--- app/src/main/java/com/example/homescreen/lowerpanel.kt
+++ app/src/main/java/com/example/homescreen/lowerpanel.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
@@ -15,73 +16,111 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 @Composable
 fun LowerPanel() {
     Column {
         WeeklySpecialCard()
-        MenuDish()
+        LazyRow {
+            items(listOf("Indian", "Chinese", "Italian", "Mexican","american","British")) { category ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Text(
+                        text = category,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(8.dp)
+                    )
+                }
+            }
+        }
+Menudish()
     }
 }
+
+
 @Composable
-fun WeeklySpecialCard(){
+fun WeeklySpecialCard() {
     Card(
         modifier = Modifier
             .fillMaxWidth()
     ) {
         Text(
             text = "Weekly Special",
-            fontSize = 26.sp,
+            fontSize = 18.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier
                 .padding(8.dp)
         )
     }
 }
-@Composable
-fun MenuDish(){
-    LazyColumn {
-        items(10) {
-            Card {
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                ) {
-                    Column {
-                        Text(text = "Greek Salad",
-                            fontSize = 18.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                        Text(text = "The famous greek salad of " +
-                                "crispy lettuce, peppers, olives, " +
-                                "our Chicago ...",
-                            color = Color.Gray,
-                            modifier = Modifier
-                                .padding(top = 5.dp, bottom = 5.dp)
-                                .fillMaxWidth(.75f)
-                        )
-                        Text(text = "$12.99",
-                            color = Color.Gray,
-                            fontWeight = FontWeight.Bold
-                        )
+    @Composable
+    fun Menudish() {
+        LazyColumn {
+            items(
+                listOf(
+                    "Chicken Tikka Masala",
+                    "Butter Chicken",
+                    "Biryani",
+                    "Naan",
+                    "Samosa",
+                    "Palak Paneer",
+                    "Dosa",
+                )
+            ) { dish ->
+                Card {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Column {
+                            Image(
+                                painter = painterResource(
+                                    id = when (dish) {
+                                        "Chicken Tikka Masala" -> R.drawable.chicken
+                                        "Butter Chicken" -> R.drawable.butter_chicken
+                                        "Biryani" -> R.drawable.biryani
+                                        "Naan" -> R.drawable.naan
+                                        "Samosa" -> R.drawable.samosa
+                                        "Palak Paneer" -> R.drawable.palak_paneer
+                                        "Dosa" -> R.drawable.dosa
+                                        else -> R.drawable.chicken
+                                    }
+                                ),
+                                contentDescription = "",
+                            )
+                            Text(
+                                text = dish,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                text = "A delicious and flavorful dish of ${dish}.",
+                                color = Color.Gray,
+                                modifier = Modifier
+                                    .padding(top = 5.dp, bottom = 5.dp)
+                                    .fillMaxWidth(.75f)
+                            )
+                            Text(
+                                text = "$${(10..25).random()}",
+                                color = Color.Gray,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
                     }
-                    Image(painter = painterResource(id = R.drawable.img),
-                        contentDescription = "",
+                    Divider(
+                        modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+                        color = Color.LightGray,
+                        thickness = 1.dp
                     )
                 }
-                Divider(modifier = Modifier.padding(start = 8.dp, end = 8.dp),
-                    color = Color.LightGray,
-                    thickness = 1.dp
-                )
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun LowerPanelPreview(){
-    LowerPanel()
-}
